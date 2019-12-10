ac74ec7e1217ad6433714af1b21167544f13e55f
timi2627 <58686879+timi2627@users.noreply.github.com>
2019-12-10 13:51:02 +0100

    ok

---
+++ app/src/main/java/com/example/androidstudio/asteroids/Model.java
@@ -0,0 +1,63 @@
+package com.example.androidstudio.asteroids;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by androidstudio on 14.01.16.
+ */
+
+public class Model {
+
+    // private Resources resources;
+
+    public SpaceShip spaceShip;
+    public Bullet bullet;
+    public Bullet bull1;
+    public Bullet bull2;
+    public Asteroid asteroid;
+
+    public static final float ticDurationS = 0.01f;
+
+    // logische screen Werte
+    public static final int screenHeight = 1000;  // logische Hoehe, die ist fix !!!, hierauf wird intern gemappt
+    public int screenWidth = 0;            // ergibt sich aus dem AspectRatio des aktuellen canvas
+
+    public ArrayList<Bullet> arBullets = new ArrayList<Bullet>();
+    public ArrayList<Asteroid> arAsteroids = new ArrayList<Asteroid>();
+
+    public Model() {
+
+        // resources = res;
+
+//        bullet = new Bullet(200f + 55f, 100f + 50f, 0f, 10f, 400, this);
+//        bullet.collision(bullet);
+//        bull1 = new Bullet(5f, 995f, 0f, 10f, 400, this);
+//        bull2 = new Bullet(screenWidth - 5f, 995f, 0f, -10f, 400, this);
+
+    }
+
+    public void init() {
+        spaceShip = new SpaceShip(screenWidth/2, screenHeight/2, this);
+        asteroid = new Asteroid(500f, 55f, 0f, -50f, this);
+    }
+
+    public void add(Bullet bullet) {  // wird von SpaceShip gefuellt -> ## Listener ?
+        arBullets.add(bullet);
+    }
+
+    public void deleteDead(){
+        Iterator<Bullet> it = arBullets.iterator();
+
+        while(it.hasNext()){
+            Bullet bullet = it.next();
+            if( ! bullet.isAlive ){
+                it.remove();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidstudio/asteroids/Screen.java
@@ -0,0 +1,63 @@
+package com.example.androidstudio.asteroids;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * der verfuegbare Screen wird auf eine Hoehe von 1000 gemappt. Die Breite ergibt sich dann
+ *   entsprechend.
+ *
+ */
+
+public class Screen extends View {
+
+    Model model;
+
+    // nur zum Test##:
+    Paint paint = new Paint();
+
+    public Screen(Context context) {
+
+        super(context);
+        setBackgroundColor(Color.BLACK);
+
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.YELLOW);
+
+     }
+
+    public void setModel(Model m) {
+        model = m;
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        // Skalierung auf 1000 virtuelle Pixel Hoehe
+        Matrix matCanvas = new Matrix();
+        float yFac = (float)getHeight()/1000f;
+        matCanvas.setScale(yFac, yFac);
+        canvas.concat(matCanvas);
+
+        model.spaceShip.draw(canvas);
+        // model.bullet.draw(canvas);
+        // model.bull1.draw(canvas);
+        // model.bull2.draw(canvas);
+
+        // ## zur Zeit gibts noch keine Asteroids-Bitmap deswegen
+        //      erscheint auch hier ein SpaceShip
+        model.asteroid.draw(canvas);
+
+        for (Bullet bullet: model.arBullets)
+        {
+            bullet.draw(canvas);
+        }
+
+        }
+}
